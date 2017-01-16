7174ffb4dde722d5a9fd15b734052dd915875507
RoN CoheN <ron.coen@gmail.com>
2017-01-17 00:03:36 +0200

    minor - remove redundant drop query,

--- models/user/user-access-rules.sql
+++ models/user/user-access-rules.sql
@@ -2,7 +2,6 @@ grant select on table {0}.{1} to active_user, inactive_user;
 
 ALTER TABLE {0}.{1} ENABLE ROW LEVEL SECURITY;
 
-drop policy if exists {1}_select_unsecure on {0}.{1};
 create policy {1}_select_unsecure on {0}.{1} for select
   using (_id::text = (select current_setting('jwt.claims._id')));
 
